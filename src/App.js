03c09f68f5d6195dbe7f9762db1835b857b9ff83
victorevh <victor_oliveirasantos@hotmail.com>
2023-05-16 13:07:41 -0300

    Adiciona component form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Banner from './components/Banner';
-import TextField from './components/TextField';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <TextField />
+      <Form />
     </div>
   );
 }
